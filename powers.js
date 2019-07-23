78328ee905c665220d530bcd4643e2ed96b5daa6
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-07-23 15:55:08 -0500

    Initial Commit

---
+++ powers.js
@@ -0,0 +1,8 @@
+document.querySelector("#activate-flight").addEventListener("click", function () {
+
+})
+
+function flightHandlerFunction (params) {
+    //remove existing class
+    //add new class
+}
